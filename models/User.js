4680eda4612ccc94799fb67ca06c9ed65d0b823b
Nick Swider <nick@fleet54.com>
2014-06-11 00:58:35 +0000

    Add mongoose; User model; registration form and route

---
+++ models/User.js
@@ -0,0 +1,8 @@
+var mongoose = require('mongoose');
+
+var User = mongoose.model('User', {
+  name: String,
+  email: String
+});
+
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/BuzzerTest');
+var User = require('../models/User');
+
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+/* POST register. */
+router.post('/register', function(req, res){
+  var user = new User({
+    name: req.body.name,
+    email: req.body.email
+  });
+  user.save(function(err){
+    if(err){ console.log('Error inserting user', err); next(); }
+    res.render('members', { title: 'Members Home'});
+  });
 });
 
 module.exports = router;
